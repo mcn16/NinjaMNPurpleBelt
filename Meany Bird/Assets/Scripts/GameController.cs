df3204cbf5709729e69b8a27ca00154145fd4280
DaKillerDonut <dylantaocallahan@gmail.com>
2025-04-12 14:04:18 -0400

    only commit

--- Meany Bird/Assets/Scripts/GameController.cs
+++ Meany Bird/Assets/Scripts/GameController.cs
@@ -5,15 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject gameOverCanvas;
+    public GameObject scoreCanvas;
+    public GameObject spawner;
     // Start is called before the first frame update
     void Start()
     {
-
+        Time.timeScale = 1;
+        scoreCanvas.SetActive(true);
+        gameOverCanvas.SetActive(false);
+        spawner.SetActive(true);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+public void GameOver() {
+        gameOverCanvas.SetActive(true);
+        spawner.SetActive(false);
+        Time.timeScale = 0;
     }
 }
