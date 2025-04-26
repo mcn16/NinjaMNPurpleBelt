ee1b1e9f8ac9fa2906d909090f7490dcbbe243f8
idkwhatimdealingwith <quinnsidell@icloud.com>
2025-04-26 13:58:03 -0400

    start the sketch

---
+++ Sketch Head/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header ("Target Object")]
+    public Transform target;
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.position.y > transform.position.y) {
+            transform.position = new Vector3(0,target.position.y, -10f);
+        }
+    }
+}
---
+++ Sketch Head/Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Rigidbody Settings")]
+    [Tooltip("Rigidbody2D object that is stored")]
+    public Rigidbody2D rb;
+
+    [Tooltip("Downward speed of the object")]
+    public float downSpeed = 20f;
+
+    [Header("Movement Settings")]
+    [Tooltip("Movement speed of the object")]
+    public float movementSpeed = 10f;
+
+     [Tooltip("Movement direction of the object")]
+     public float movement = 0f;
+
+     private SpriteRenderer spriteRenderer
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizatal") * movementSpeed;
+        spriteRenderer.flipX = movement < 0;
+    }
+}
